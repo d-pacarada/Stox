189dd0ea2007867bdde19da50002759ecceec492
mdesign-ks <mekro112@gmail.com>
2025-04-10 22:13:53 +0200

    Landingpage

--- STOX/src/App.jsx
+++ STOX/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import LandingPage from './Landingpage';
 import SignUpForm from './SignUpForm';
 import StepTwo from './StepTwo'; // We’ll create this next
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignUpForm />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignUpForm />} />
       <Route path="/step2" element={<StepTwo />} />
+
     </Routes>
   );
 }
